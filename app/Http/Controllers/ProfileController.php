918529e35c05b62da893caad720649b41c2e743e
Bruno Santana <bsantana.it@gmail.com>
2023-03-10 17:59:44 -0300

    ProfileController Documented

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,20 +7,40 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
+        // Retrieve all profiles from the database
         $profiles = Profile::all();
 
+        // Renders the profile.index view with retrieved profiles
         return view('profile.index', compact('profiles'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create()
     {
+        // Renders the profile.create view to create a new profile
         return view('profile.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
+        // Validate required fields
         $request->validate([
             'first_name' => 'required',
             'last_name' => 'required',
@@ -28,23 +48,47 @@ class ProfileController extends Controller
             'gender' => 'required',
         ]);
 
+        // Creates a new profile with request data
         Profile::create($request->all());
 
+        // Redirects to profile listing with success message
         return redirect()->route('profile.index')->with('success', 'Profile created successfully.');
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\View\View
+     */
     public function show(Profile $profile)
     {
+        // Renders the profile.show view with the specific profile passed as a parameter
         return view('profile.show', compact('profile'));
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\View\View
+     */
     public function edit(Profile $profile)
     {
+        // Renders the profile.edit view with the specific profile passed as a parameter for editing
         return view('profile.edit', compact('profile'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request, Profile $profile)
     {
+        // Validate required fields
         $request->validate([
             'first_name' => 'required',
             'last_name' => 'required',
@@ -52,13 +96,22 @@ class ProfileController extends Controller
             'gender' => 'required',
         ]);
 
+        // Updates the profile with the request data
         $profile->update($request->all());
 
+        // Redirects to profile listing with success message
         return redirect()->route('profile.index')->with('success', 'Profile updated successfully.');
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function destroy(Profile $profile)
     {
+        // Remove the profile
         $profile->delete();
 
         return redirect()->route('profile.index')->with('success', 'Profile deleted successfully.');
